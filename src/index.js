2017c1d8821deb2130d27d4872a87e2a7ce57c13
Johan Roxendal <johan.roxendal@gu.se>
2019-01-20 21:36:01 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import express from "express"
+import crawler from "./crawler"
+
+import puppeteer from "puppeteer"
+import url from "url"
+
+let browser = null
+
+
+const app = express()
+app.get("*", async function(req, res, next) {
+    if(!browser) {
+        browser = await puppeteer.launch({ args: ["--no-sandbox"] })
+    }
+    let path = url.parse(req.originalUrl).pathname
+    console.log("path", path)
+    const from = "https://litteraturbanken.se" + path
+    console.time("fetch")
+    let html = await crawler({ url : from, browser})
+    console.timeEnd("fetch") 
+    res.type('html')
+    res.send(html)
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}.`))
+
+
+async function main() {
+    console.time("browser start")
+    
+    console.timeEnd("browser start")
+    const url = "https://litteraturbanken.se/forfattare/HanssonGD/titlar/Senecaprogrammet/sida/105/etext"
+    const url2 = "https://litteraturbanken.se/forfattare/HanssonGD/titlar/Senecaprogrammet/sida/106/etext"
+    console.time("first")
+    var html = await crawler({ url, browser})
+    console.timeEnd("first")
+    console.log("html", html.length)
+    console.time("second")
+    html = await crawler({ url : url2, browser})
+    console.timeEnd("second")
+    console.log("html", html.length)
+    await browser.close()
+    console.log("done.")
+}
+
+async function test2() {
+    console.time("browser start")
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] })
+    console.timeEnd("browser start")
+    const url = "https://litteraturbanken.se/forfattare/HanssonGD/titlar/Senecaprogrammet/sida/105/etext"
+    const url2 = "https://litteraturbanken.se/forfattare/HanssonGD/titlar/Senecaprogrammet/sida/106/etext"
+    var first = crawler({ url, browser})
+    var second = crawler({ url : url2, browser})
+    console.time("both")
+    let [firstHtml, secondHtml] = await Promise.all([first, second])
+    console.timeEnd("both")
+    console.log(firstHtml.length, secondHtml.length)
+
+    await browser.close()
+    console.log("done.")   
+}
+
+// main()
+
+// test2()
